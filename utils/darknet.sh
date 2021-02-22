03c95233ee883012949030d46fcf65304289b98c
Christopher Mayershofer <christopher.mayershofer@tum.de>
2021-02-22 18:55:17 +0100

    Add synthetic images (v3)

--- utils/darknet.sh
+++ utils/darknet.sh
@@ -12,16 +12,19 @@ find subset-3 -name *.jpg -exec mv {} train/ \;
 find subset-4 -name *.jpg -exec mv {} val/ \;
 find subset-5 -name *.jpg -exec mv {} train/ \;
 
-wget https://syncandshare.lrz.de/dl/fiFYUe1WeCczJnApvrKdusgo/loco-v2-darknet-6c.zip
-unzip loco-v2-darknet-6c.zip
+wget https://syncandshare.lrz.de/dl/fiQorKfVC8GYBsfSaVWxiRPa/loco-v3-darknet-6c.zip
+unzip loco-v3-darknet-6c.zip
 
 mv train.6c/* train/
 mv val.6c/* val/
+mv synth.6c/* synth/
 
 find train/ -name *.jpg > manifest_train.txt
 find val/ -name *.jpg > manifest_val.txt
+find synth/ -name *.jpg > manifest_synth.txt
+
 
 
 # Clean up
-rm subset-* train.6c val.6c all.6c loco-v2-darknet-6c.zip -rf
+rm subset-* train.6c val.6c all.6c synth.6c loco-v3-darknet-6c.zip -rf
 
