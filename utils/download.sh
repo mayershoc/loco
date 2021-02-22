9ea9504e9895b5bc6c20505b7655b63c18840386
Christopher Mayershofer <christopher.mayershofer@tum.de>
2021-02-22 18:55:17 +0100

    Add synthetic images (v3)

--- utils/download.sh
+++ utils/download.sh
@@ -6,3 +6,8 @@ cd $DIR/../
 wget https://go.mytum.de/239870 -O dataset.zip
 unzip dataset.zip
 rm dataset.zip
+
+cd dataset
+wget https://syncandshare.lrz.de/dl/fi2X1cXzVenDkm3YKHxznrgq/synth.zip -O synth.zip
+unzip synth.zip
+rm synth.zip
